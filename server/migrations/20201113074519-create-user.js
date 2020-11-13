65fadca594274834279a58b4a88a5e6aeb463873
Samuel Aditia <samueladitia95@gmail.com>
2020-11-13 21:37:55 +0700

    1. Add bcrypt and jwt helpers 2. Add sequelize validation in user, savedConfig, recommendedConfig, and game

--- server/migrations/20201113074519-create-user.js
+++ server/migrations/20201113074519-create-user.js
@@ -1,39 +1,42 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      username: {
-        type: Sequelize.STRING
-      },
-      firstname: {
-        type: Sequelize.STRING
-      },
-      lastname: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable("Users", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			username: {
+				type: Sequelize.STRING,
+			},
+			firstname: {
+				type: Sequelize.STRING,
+			},
+			lastname: {
+				type: Sequelize.STRING,
+			},
+			email: {
+				type: Sequelize.STRING,
+			},
+			password: {
+				type: Sequelize.STRING,
+			},
+			is_admin: {
+				type: Sequelize.BOOLEAN,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable("Users");
+	},
 };
--- server/models/game.js
+++ server/models/game.js
@@ -1,25 +1,76 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
-  class Game extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };Game.init({
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    picture_url: DataTypes.STRING,
-    rating: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Game',
-  });
-  return Game;
+	class Game extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Game.init(
+		{
+			title: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notEmpty: {
+						msg: "Game Title is Required",
+					},
+					notNull: {
+						msg: "Game Title is Required",
+					},
+				},
+			},
+			description: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notEmpty: {
+						msg: "Game Description is Required",
+					},
+					notNull: {
+						msg: "Game Description is Required",
+					},
+				},
+			},
+			picture_url: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notEmpty: {
+						msg: "Game Picture URL is Required",
+					},
+					notNull: {
+						msg: "Game Picture URL is Required",
+					},
+					isUrl: {
+						msg: "Game Picture URL is Invalid",
+					},
+				},
+			},
+			rating: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notEmpty: {
+						msg: "Rating is Required",
+					},
+					min: {
+						args: 1,
+						msg: "Rating is Invalid",
+					},
+				},
+			},
+		},
+		{
+			sequelize,
+			modelName: "Game",
+		},
+	);
+	return Game;
 };
