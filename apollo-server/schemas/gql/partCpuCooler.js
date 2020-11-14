cfb761ebab64905463a3eb89bc7d9f6985303302
ridwan11dvk <ridwan11depok@gmail.com>
2020-11-14 16:30:29 +0700

    lanjutin apollo

---
+++ apollo-server/schemas/gql/partCpuCooler.js
@@ -0,0 +1,11 @@
+const partCPUCooler = `
+    id: ID
+    name: String
+    socket:[Socket]
+    TDP: Int
+    manufacturer: String
+    power_draw: Int
+    price: Int
+    picture_url: String
+`;
+module.exports = partCPUCooler;
