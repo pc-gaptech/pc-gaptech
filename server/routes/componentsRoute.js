2c8ff90f659a15d9b65973b3156f255931dd30f9
Samuel Aditia <samueladitia95@gmail.com>
2020-11-14 22:22:13 +0700

    Finaliza Server Alpha version

--- server/routes/componentsRoute.js
+++ server/routes/componentsRoute.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
 const ComponentsController = require("../controllers/componentsController");
+const { authorizationAdmin } = require("../middlewares/auth");
 
 router.get("/", ComponentsController.getAll);
 router.get("/:component", ComponentsController.getAllOneType);
 router.get("/:component/:id/detail", ComponentsController.getOne);
-router.post("/:component/add", ComponentsController.addOne);
-router.delete("/:component/:id/delete", ComponentsController.deleteOne);
-router.put("/:component/:id/update", ComponentsController.updateOne);
+router.post("/:component/add", authorizationAdmin, ComponentsController.addOne);
+router.delete("/:component/:id/delete", authorizationAdmin, ComponentsController.deleteOne);
+router.put("/:component/:id/update", authorizationAdmin, ComponentsController.updateOne);
 
 module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,14 +3,20 @@ const componentRoute = require("./componentsRoute");
 const favoritesRoute = require("./favoritesRoute");
 const gamesRoute = require("./gamesRoute");
 const userRoute = require("./userRoute");
+const recommendpc = require("./recommendedRoute");
 
 const CheckConfigController = require("../controllers/checkConfigController");
+const { authentication } = require("../middlewares/auth");
 
 router.use("/", userRoute);
+
 router.post("/checkconfig", CheckConfigController.check);
+
+router.use(authentication);
 
 router.use("/parts", componentRoute);
 router.use("/favorites", favoritesRoute);
 router.use("/games", gamesRoute);
+router.use("/recommendpc", recommendpc);
 
 module.exports = router;
