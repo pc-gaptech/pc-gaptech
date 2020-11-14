6dc98300ebe6f409662e55ac2ac5c4444d225012
ridwan11dvk <ridwan11depok@gmail.com>
2020-11-14 19:21:54 +0700

    fetch data by component

--- apollo-server/schemas/gql/partMotherdboard.js
+++ apollo-server/schemas/gql/partMotherdboard.js
@@ -1,11 +1,13 @@
-const partCPUCooler = `
+const partMotherboard = `
     id: ID
     name: String
     socket: [Socket]
-    TDP: Int
+    chipset: [Chipset]
+    form_factor: [FormFactor]
     manufacturer: String
     power_draw: Int
     price: Int
     picture_url: String
 `;
-module.exports = partCPUCooler;
+
+module.exports = partMotherboard;
